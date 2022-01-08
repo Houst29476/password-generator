4d74bdc90e1679a3437893b65e3938b2c29c7908
Bradley Boyd <bradley.boyd@gmail.com>
2022-01-08 17:01:05 -0600

    setup variables for arrays

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Variables for Array
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","<",">","."];
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+}  
+
+function generatePassword () {
+
+  var passwordHolder = [];
+
+  var password = [];
+
+  var userInput = parseInt(prompt("Choose a number between 5 and 120."));
+
+
+
+
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+var passwordText = document.querySelector("#password");
+
+// Pseudo Code
+// 1. create arrays for letters, numbers, and special characters.
+// 2. create a prompt for user input how many characters they want for their password.
+// 3. create a filter (confirm) for which characters they want to include or exclude.
+// 4. create a variable to contain the user input
+// 5. create a for loop(s) to generate the randomize password 
+// 6. then push the ranomdized generated password to the text box.
